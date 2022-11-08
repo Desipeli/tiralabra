b357a871fdcc6a32df244865a77145ce90f03282
Desipeli <mr.desipeli@gmail.com>
2022-11-08 19:50:01 +0200

    coverage käyttöön

--- lausegeneraattori/tasks.py
+++ lausegeneraattori/tasks.py
@@ -3,3 +3,7 @@ from invoke import task
 @task
 def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
+
+@task
+def testcov(ctx):
+    ctx.run("coverage run --branch -m pytest; coverage html", pty=True)
