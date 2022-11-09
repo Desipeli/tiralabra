9b504844a54e2f5f985f1e193e8052730499c6fe
Desipeli <mr.desipeli@gmail.com>
2022-11-09 11:14:48 +0200

    docstring

--- lausegeneraattori/src/jasennin.py
+++ lausegeneraattori/src/jasennin.py
@@ -1,5 +1,15 @@
 class Jasennin:
 
+    """
+    Erottaa välimerkit (.,!?) sanoista, ja tallentaa jokaisen
+    sanan ja välimerkin omaksi alkiokseen listaan
+
+    Parametrit:
+        teksti: Merkkijono jota käsitellään
+    
+    Palauttaa listan sanoista ja välimerkeistä
+    """
+
     def jasenna_listaksi(self, teksti):
         jasennetty = teksti.replace(
             "\n", " ").replace(
