16d9382c487f06de91d097934103dbf2877b324f
Desipeli <mr.desipeli@gmail.com>
2022-11-09 13:49:10 +0200

    Ohjelma erikseen

--- lausegeneraattori/src/index.py
+++ lausegeneraattori/src/index.py
@@ -1,75 +1,16 @@
-import os.path
 from tietorakenteet.trie import Trie
 from konsoli import Konsoli
 from tiedostonlukija import TiedostonLukija
 from jasennin import Jasennin
 from arpa import Arpa
+from ohjelma import Ohjelma
 
-
-def paavalikon_ohjeet():
-    print("1: valitse aste")
-    print("2: lisää kansiosta 'data' tiedoston sisältö")
-    print("3: tuota lause!")
-    print("q: lopeta")
-
-
-def pilko_syotettava_data_asteen_mukaisesti(data, aste):
-    """ Pilkotaan data listoihin asteen mukaan """
-    lista = []
-    aste = aste + 1
-    if aste > len(data):
-        print("asteen oltava pienempi kuin syötteen pituus")
-        return False
-
-    for i in range(len(data) - aste + 1):
-        sisalista = []
-        kohta = i
-        while kohta < i + aste and kohta < len(data):
-            print(data[kohta])
-            sisalista.append(data[kohta])
-            kohta += 1
-        lista.append(sisalista)
-        print()
-    return lista
-
-def laheta_trielle(trie, lista):
-    pass
-
-def ohjelma():
-    io = Konsoli()
-    tiedoston_lukija = TiedostonLukija()
-    jasennin = Jasennin()
-    trie = Trie(Arpa())
-    aste = 2
-    paavalikon_ohjeet()
-
-    while True:
-        syote = io.lue("toiminto: ")
-
-        if syote == "1":
-            uusi_aste = io.lue("Aseta uusi aste (tiedostot ladattava uudestaan): ")
-            try:
-                uusi_aste_numerona = int(uusi_aste)
-                if uusi_aste_numerona >= 0:
-                    aste = uusi_aste_numerona
-            except:
-                io.kirjoita("Syötä vain positiivisia kokonaislukuja tai 0")
-                continue
-        elif syote == "2":
-            tiedoston_sisalto = tiedoston_lukija.lue("dracula.txt")
-            print(jasennin.jasenna_listaksi(tiedoston_sisalto))
-        elif syote == "3":
-            io.kirjoita("lause")
-        elif syote == "q":
-            break
-        elif syote == "t":
-            trie.syvyyspuu()
-        else:
-            pilko_syotettava_data_asteen_mukaisesti(
-                [1, 2, 3, 4 ,5],
-                aste
-            )
-            paavalikon_ohjeet()
 
 if __name__ == "__main__":
-    ohjelma()
+
+    Ohjelma(
+        Konsoli(),
+        TiedostonLukija(),
+        Jasennin(),
+        Arpa()
+    ).kaynnista()
---
+++ lausegeneraattori/src/ohjelma.py
@@ -0,0 +1,115 @@
+from tietorakenteet.trie import Trie
+from konsoli import Konsoli
+from tiedostonlukija import TiedostonLukija
+from jasennin import Jasennin
+from arpa import Arpa
+
+
+class Ohjelma:
+    """ Pääohjelma """
+
+    def __init__(self,
+        konsoli: "Konsoli",
+        tiedostonlukija: "TiedostonLukija",
+        jasennin: "Jasennin",
+        arpa: "Arpa"
+    ):
+        """
+        Konstruktori
+
+        Parametrit:
+            konsoli: tulostukseen ja syötteiden lukuun
+            tiedostonlukija: Palauttaa tiedoston sisällön merkkijonona
+            jasennin: datan muuttaminen sopivaan muotoon
+            arpa: satunnaislukugeneraattori
+        """
+        self._trie = Trie(arpa)
+        self._jasennin = jasennin
+        self._tiedostonlukija = tiedostonlukija
+        self._konsoli = konsoli
+        self._arpa = arpa
+
+        self._aste = 0
+
+    def _paavalikon_ohjeet(self):
+        self._konsoli.kirjoita("1: valitse aste")
+        self._konsoli.kirjoita("2: lataa tiedosto")
+        self._konsoli.kirjoita("3: tulosta lause")
+        self._konsoli.kirjoita("q: lopeta")
+        self._konsoli.kirjoita("t: tulosta trie")
+
+    def kaynnista(self):
+        """
+        Ohjelman pääsilmukka
+
+        Pyydetään käyttäjältä syötettä ja toimitaan sen mukaisesti.
+        """
+        self._paavalikon_ohjeet()
+
+        while True:
+            syote = self._konsoli.lue("toiminto: ")
+            if syote == "1":
+                self._valitse_aste()
+            elif syote == "2":
+                self._lataa_tiedosto()
+            elif syote == "3":
+                self._konsoli.kirjoita("lause")
+            elif syote == "t":
+                self._konsoli.kirjoita(self._trie.syvyyspuu())
+                self._konsoli.kirjoita(len(self._trie.syvyyspuu()))
+            elif syote == "q":
+                break
+
+    def _valitse_aste(self):
+        """ Uuden asteen valinta """
+        self._konsoli.kirjoita(f"nykyinen aste: {self._aste}")
+        syote = self._konsoli.lue("syötä uusi aste (pos. kokonaisluku): ")
+
+        try:
+            syote = int(syote)
+        except ValueError:
+            self._konsoli.kirjoita("virhe: syötteen oltava kokonaisluku")
+            self._konsoli.kirjoita("astetta ei vaihdettu")
+            self._paavalikon_ohjeet()
+            return
+
+        if syote < 0 or syote == self._aste:
+            self._konsoli.kirjoita("astetta ei vaihdettu")
+            self._paavalikon_ohjeet()
+        else:
+            self._trie = Trie(self._arpa)
+            self._aste = syote
+            self._konsoli.kirjoita(f"aste vaihdettu: {self._aste}")
+            self._konsoli.kirjoita("syötteet ladattava uudestaan")
+            self._paavalikon_ohjeet()
+
+    def _lataa_tiedosto(self):
+        """
+        Ladataan tiedosto, jonka sisältö jäsennetään ja lisätään
+        trieen.
+        """
+        tiedoston_nimi = self._konsoli.lue("tiedoston nimi: ")
+        sisalto = self._tiedostonlukija.lue(tiedoston_nimi)
+        lista = self._jasennin.jasenna_listaksi(sisalto)
+        self._pilko_ja_laheta_trielle(lista)
+        self._paavalikon_ohjeet()
+
+    def _pilko_ja_laheta_trielle(self, data):
+        """
+        Valitaan listasta aseen pituiset sanajonot ja lähetetään ne
+        trielle.
+
+        Parametrit:
+            data: lista sanoja ja välimerkkejä
+        """
+        aste = self._aste + 1
+        if aste > len(data):
+            self._konsoli.kirjoita("asteen oltava pienempi kuin syötteen pituus")
+            return
+        for i in range(len(data) - aste + 1):
+            sanalista = []
+            dataosoitin = i
+            while dataosoitin < i + aste and dataosoitin < len(data):
+                sanalista.append(data[dataosoitin])
+                dataosoitin += 1
+            self._trie.lisaa_sanalista(sanalista)
