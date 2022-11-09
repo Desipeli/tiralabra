de35a2b012f90659fcdee59316607bf28f8e24bd
Desipeli <mr.desipeli@gmail.com>
2022-11-09 11:10:32 +0200

    docstring

--- lausegeneraattori/src/tiedostonlukija.py
+++ lausegeneraattori/src/tiedostonlukija.py
@@ -2,7 +2,15 @@ import os.path
 
 
 class TiedostonLukija:
-    """ Ohjelmalle syötettävän datan lukeminen ja jäsennys """
+    """
+    Ohjelmalle syötettävän tiedoston lukeminen
+
+    Parametrit:
+        tiedosto: data-kansiossa olevan tiedoston nimi
+
+    Palauttaa tiedoston sisällön merkkijonona
+
+    """
 
     def lue(self, tiedosto):
         sisalto = open(os.path.dirname(__file__)+"/data/"+tiedosto,
