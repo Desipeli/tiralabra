82c685b17d08026accaaec4d7a291fcbdc4e2082
Desipeli <mr.desipeli@gmail.com>
2022-11-16 11:31:25 +0200

    Sanoista koostuvan listan muuttaminen merkkijonoksi

--- lausegeneraattori/src/jasennin.py
+++ lausegeneraattori/src/jasennin.py
@@ -1,21 +1,46 @@
 class Jasennin:
 
-    """
-    Erottaa välimerkit (.,!?) sanoista, ja tallentaa jokaisen
-    sanan ja välimerkin omaksi alkiokseen listaan
+    def jasenna_listaksi(self, teksti: str):
+        """
+        Erottaa välimerkit (.,!?) sanoista, ja tallentaa jokaisen
+        sanan ja välimerkin omaksi alkiokseen listaan
 
-    Parametrit:
-        teksti: Merkkijono jota käsitellään
+        Parametrit:
+            teksti: Merkkijono jota käsitellään
 
-    Palauttaa listan sanoista ja välimerkeistä
-    """
-
-    def jasenna_listaksi(self, teksti):
+        Palauttaa listan sanoista ja välimerkeistä
+        """
         jasennetty = teksti.replace(
             "\n", " ").replace(
                 ".", " . ").replace(
                     ",", " , ").replace(
                         "!", " ! ").replace(
                             "?", " ? ").split()
+
+        return jasennetty
+
+    def jasenna_tekstiksi(self, lista: list):
+        """
+        Muuttaa listan merkkijonoksi, ja poistaa välimerkkien
+        edestä välilyönnin
+
+        Parametrit:
+            lista: lista sanoja ja välimerkkejä
+
+        Palauttaa merkkijonon
+        """
+        jasennetty = " ".join(lista).replace(
+            " .", "."
+            ).replace(
+                " ,", ","
+            ).replace(
+                " !", "!"
+            ).replace(
+                " ?", "?"
+            ).replace(
+                '"', ""
+            ).replace(
+                "'", ""
+            )
 
         return jasennetty
