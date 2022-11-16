6b65ea3ad88ef1cbf18cd57202db8ea78ec99cb9
Desipeli <mr.desipeli@gmail.com>
2022-11-16 12:13:53 +0200

    docstring

--- lausegeneraattori/src/arpa.py
+++ lausegeneraattori/src/arpa.py
@@ -5,7 +5,16 @@ from random import choice
 class Arpa:
     """Satunnaisuudet"""
     def arvo_kokonaisluku(self, a, b):
+        """ Arvotaan kokonaisluku kahden luvun väliltä """
         return randint(a, b)
 
     def arvo_joukosta(self, alkiot):
-        return choice(list(alkiot))
+        """ Arvotaan jokin joukossa esiintyvä alkio """
+        valinta = None
+        try:
+            valinta = choice(list(alkiot))
+        except IndexError as error:
+            print(error)
+        except TypeError as error:
+            print(error)
+        return valinta
