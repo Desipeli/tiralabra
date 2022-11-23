29e91a5b43ff5a3e12cfc01252becb86e313eba1
Desipeli <mr.desipeli@gmail.com>
2022-11-23 16:02:49 +0200

    Mahdollisuus valita käyttöliittymä

--- lausegeneraattori/src/index.py
+++ lausegeneraattori/src/index.py
@@ -1,3 +1,4 @@
+import sys
 from tietorakenteet.trie import Trie
 from konsoli import Konsoli
 from tiedostonlukija import TiedostonLukija
@@ -8,20 +9,22 @@ from tekstikayttoliittyma import TekstiKayttoliittyma
 from gui import GUI
 
 
+
 if __name__ == "__main__":
     arpa = Arpa()
     konsoli = Konsoli()
     tiedostonlukija = TiedostonLukija()
     jasennin = Jasennin()
+    ohjelma = Ohjelma(jasennin, arpa)
 
-    ohjelma = Ohjelma(
-        jasennin,
-        arpa
-    )
+    print(len(sys.argv))
 
-    # kayttoliittyma = TekstiKayttoliittyma(
-    #     konsoli,
-    #     tiedostonlukija,
-    #     ohjelma
-    # ).kaynnista()
-    kayttoliittyma = GUI(ohjelma, tiedostonlukija) 
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "text":
+            TekstiKayttoliittyma(
+                konsoli,
+                tiedostonlukija,
+                ohjelma
+            ).kaynnista()
+            sys.exit(0)
+    GUI(ohjelma, tiedostonlukija)
