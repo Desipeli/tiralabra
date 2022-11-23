f6d868abeccc0c75cf6b2db64094e3d225c6f7df
Desipeli <mr.desipeli@gmail.com>
2022-11-23 16:02:20 +0200

    Lisää ominaisuuksia

--- lausegeneraattori/src/gui.py
+++ lausegeneraattori/src/gui.py
@@ -25,12 +25,22 @@ class GUI:
             command=self.lataa_tiedostoja)
         nappi_aste = tk.Button(self.toimintokehys, text="Vaihda aste", command=self.vaihda_aste)
         self.teksti_aste = tk.Label(self.toimintokehys, text=f"aste: ")
+        self.teksti_solmut = tk.Label(self.toimintokehys, text="Triessä solmuja: 0")
         nappi_lause = tk.Button(self.toimintokehys, text="Muodosta Lause", command=self.muodosta_lause)
+        tarinan_pituus = tk.Label(self.toimintokehys, text="tarinan pituus min")
+        self.input_tarinan_pituus = tk.Entry(self.toimintokehys, text="20")
+        nappi_tarina = tk.Button(self.toimintokehys, text="Muodosta tarina", command=self.muodosta_tarina)
+        nappi_kopioi = tk.Button(self.toimintokehys, text="Kopioi", command=self.kopioi_teksti)
 
         nappi_lataa.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
         self.teksti_aste.grid(row=1, column=0, sticky="ew", padx=5)
-        nappi_aste.grid(row=2, column=0, sticky="ew", padx=5)
-        nappi_lause.grid(row=3, column=0, sticky="ew", padx=5)
+        self.teksti_solmut.grid(row=2, column=0, sticky="ew", padx=5)
+        nappi_aste.grid(row=3, column=0, sticky="ew", padx=5)
+        nappi_lause.grid(row=4, column=0, sticky="ew", padx=5)
+        tarinan_pituus.grid(row=5, column=0, sticky="ew", padx=5)
+        self.input_tarinan_pituus.grid(row=6, column=0, sticky="ew", padx=5)
+        nappi_tarina.grid(row=7, column=0, sticky="ew", padx=5)
+        nappi_kopioi.grid(row=8, column=0, sticky="ew", padx=5)
 
         self.toimintokehys.grid(row=0, column=0, sticky="ns")
         self.tekstilaatikko.grid(row=0, column=1, sticky="nsew")
@@ -40,9 +50,8 @@ class GUI:
 
     def lataa_tiedostoja(self):
         """ Voidaan ladata monta tiedostoa kerralla """
-        print(os.getcwd())
         tiedostojen_nimet = filedialog.askopenfilenames(
-            initialdir= "/",
+            initialdir= os.path.join(os.path.dirname(__file__), "data"),
             title="valitse tiedosto(t)",
             filetypes= (("Text files",
                         "*.txt*"),
@@ -55,6 +64,7 @@ class GUI:
                 print(nimi)
             else:
                 print("Virhe: ", nimi)
+        self.paivita_solmut()
 
     def vaihda_aste(self):
         """ Pyydetään käyttäjältä uusi aste """
@@ -80,8 +90,36 @@ class GUI:
             self.tekstilaatikko.insert("1.0", lause)
         else:
             messagebox.showerror(
-                message="Lauseenmuodostus ei onnistu. Oletko varmasti ladannut tiedostoja?")
+                message="Teksitn muodostus ei onnistu. Oletko varmasti ladannut tiedostoja?")
 
-if __name__ == "__main__":
-    import jasennin, arpa
-    GUI(Ohjelma(jasennin.Jasennin(), arpa.Arpa()))
+    def muodosta_tarina(self):
+        alku = self.tekstilaatikko.get("1.0", tk.END)
+        pituus = self.input_tarinan_pituus.get()
+        try:
+            pituus = int(pituus)
+        except ValueError:
+            pituus = 0
+        if pituus > 1000: pituus = 1000
+        self.paivita_pituus(pituus)
+
+        tarina = self.ohjelma.tarinan_muodostuksen_aloitus(alku, pituus)
+        print(tarina)
+        if tarina:
+            self.tekstilaatikko.delete("1.0", tk.END)
+            self.tekstilaatikko.insert("1.0", tarina)
+        else:
+            messagebox.showerror(
+                message="Tekstin muodostus ei onnistu. Oletko varmasti ladannut tiedostoja?")
+
+    def paivita_pituus(self, pituus):
+        """ Päivitetään GUI:n tarinan minimipituuskenttä """
+        self.input_tarinan_pituus.config(text=pituus)
+
+    def paivita_solmut(self):
+        """ Haetaan ohjelmasta trien solmujen lkm ja näytetään se """
+        solmuja = self.ohjelma.hae_trien_koko()
+        self.teksti_solmut.config(text=f"triessä solmuja: {solmuja}")
+
+    def kopioi_teksti(self):
+        self.ikkuna.clipboard_clear()
+        self.ikkuna.clipboard_append(self.tekstilaatikko.get("1.0", tk.END))
