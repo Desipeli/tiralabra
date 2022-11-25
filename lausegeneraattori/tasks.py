4182021e0e1c45d239809e04c6c4a3047339f9cd
Desipeli <mr.desipeli@gmail.com>
2022-11-25 13:07:04 +0200

    invoke suorituskyky

--- lausegeneraattori/tasks.py
+++ lausegeneraattori/tasks.py
@@ -9,5 +9,9 @@ def testcov(ctx):
     ctx.run("coverage run --branch -m pytest; coverage html", pty=True)
 
 @task
+def testperf(ctx):
+    ctx.run("python3 src/index.py test", pty=True)
+
+@task
 def lint(ctx):
     ctx.run("pylint src", pty=True)
