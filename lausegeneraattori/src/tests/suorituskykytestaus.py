3bf7f3ae0ca483b6b43dffea064d8ca75734fd4c
Desipeli <mr.desipeli@gmail.com>
2022-11-28 12:33:40 +0200

    refaktorointia

--- lausegeneraattori/src/tests/suorituskykytestaus.py
+++ lausegeneraattori/src/tests/suorituskykytestaus.py
@@ -24,37 +24,15 @@ class SuoritusTestaus:
         self.tiedostonlukija = TiedostonLukija()
         self.datapolku = os.path.join(os.path.dirname(__file__), "../data")
         tiedostot = self.tiedostonlukija.kaikkien_tiedostojen_nimet(self.datapolku)
-        self.alusta_tulokset(tiedostot)
 
-        #tr1 = os.path.join(self.datapolku, "taistelu_roomasta_1.txt")
+        tulokset = self.kaikkien_tiedostojen_tallennus_tyhjaan_trieen(tiedostot, 1)
+        self.kaikki_tyhjaan_trieen_kuvaaja(tulokset)
 
-        #self.tekstin_jasennys_listaksi(tiedostot)
+        tulokset = self.kaikkien_tiedostojen_tallennus_trieen(tiedostot, 1)
+        self.kaikki_trieen_kuvaaja(tulokset)
 
-        # for i in range(6):
-        #     print("tallennetaan", tr1, "asteella", i)
-        #     self.laske_trieen_talletusaika(tr1, i)
-        
-        self.kaikkien_tiedostojen_tallennus_tyhjaan_trieen(tiedostot, 4)
-        self.kaikkien_tiedostojen_tallennus_trieen(tiedostot, 4)
-
-        for avain, arvo in self.tulokset.items():
-            print(avain)
-            print(arvo)
-            print()
-            
-        # self.trieen_tallennuksen_kuvaaja()
-        # self.teksti_listaksi_kuvaaja()
-        self.kaikki_tyhjaan_trieen_kuvaaja()
-        self.kaikki_trieen_kuvaaja()
-
-    def alusta_tulokset(self, tiedostot):
-        """ Sanakirja johon tallennetaan testien tulokset """
-        self.tulokset = {}
-        self.tulokset["tiedostoja"] = len(tiedostot)
-        self.tulokset["jasennykset"] = []
-        self.tulokset["trieen"] = []
-        self.tulokset["kaikki_tyhjaan_trieen"] = []
-        self.tulokset["kaikki_trieen"] = {}
+        tulokset = self.tekstin_jasennys_listaksi(tiedostot)
+        self.teksti_listaksi_kuvaaja(tulokset)
 
     def kaikkien_tiedostojen_tallennus_tyhjaan_trieen(self, tiedostot: list, aste: int, toistot: int = 5):
         """
@@ -66,6 +44,7 @@ class SuoritusTestaus:
             aste: aste jolla tiedostojen sisältö tallennetaan
             toistot: kuinka monta kertaa operaatio tehdään. Lopuksi tallennetaan keskiarvo
         """
+        tulokset = []
         t = tiedostot.copy()
         self.ohjelma.vaihda_aste(aste)
         for i in range(len(tiedostot)):
@@ -80,80 +59,52 @@ class SuoritusTestaus:
                 alku = time()
                 self.ohjelma.lataa_tiedoston_sisalto(sisalto)
                 aikojen_summa += time() - alku
-            self.tulokset["kaikki_tyhjaan_trieen"].append((tiedosto, sanoja, aikojen_summa / toistot))
+            tulokset.append((tiedosto, sanoja, aikojen_summa / toistot))
+        return tulokset
 
     def kaikkien_tiedostojen_tallennus_trieen(self, tiedostot: list, aste: int, toistot: int = 5):
         """
         Kuin ylempi funktio, mutta trieä ei tyhjennetä välissä.
         """
+        tulokset = {}
         self.ohjelma.vaihda_aste(aste)
         for toisto in range(toistot):
             t = tiedostot.copy()
             for tiedosto in range(len(tiedostot)):
                 tiedosto = random.choice(t)
-                if tiedosto not in self.tulokset["kaikki_trieen"]:
-                    self.tulokset["kaikki_trieen"][tiedosto] = (0,0)
+                if tiedosto not in tulokset:
+                    tulokset[tiedosto] = (0,0)
                 t.remove(tiedosto)
                 polku = os.path.join(self.datapolku, tiedosto)
                 sisalto = self.tiedostonlukija.lue(polku)
                 sanoja = len(Jasennin().jasenna_listaksi(sisalto))
                 alku = time()
                 self.ohjelma.lataa_tiedoston_sisalto(sisalto)
-                self.tulokset["kaikki_trieen"][tiedosto] = (
-                    self.tulokset["kaikki_trieen"][tiedosto][0] + time() - alku,
+                tulokset[tiedosto] = (
+                    tulokset[tiedosto][0] + time() - alku,
                     sanoja)
-            print("KAIKKI", toisto, self.tulokset["kaikki_trieen"])
-        for avain, arvo in self.tulokset["kaikki_trieen"].items():
-            self.tulokset["kaikki_trieen"][avain] = (arvo[0] / toistot, arvo[1])
+            print("KAIKKI", toisto, tulokset)
+        for avain, arvo in tulokset.items():
+            tulokset[avain] = (arvo[0] / toistot, arvo[1])
+        return tulokset
 
     def tekstin_jasennys_listaksi(self, tiedostot: list):
         """ jäsennetään teksti listaksi """
+        tulokset = []
         for tiedosto in tiedostot:
             sisalto = self.tiedostonlukija.lue(os.path.join(self.datapolku, tiedosto))
             alku = time()
             lista = Jasennin().jasenna_listaksi(sisalto)
-            self.tulokset["jasennykset"].append((tiedosto, time()-alku, len(lista)))
+            tulokset.append((tiedosto, time()-alku, len(lista)))
+        return tulokset
 
-    def laske_trieen_talletusaika(self, tiedosto, aste):
-        """ Jäsennetään sisältö ohjelman funktiolle """
-        sisalto = TiedostonLukija().lue(tiedosto)
-        sanat_listana = Jasennin().jasenna_listaksi(sisalto)
-        self.pilko_ja_laheta_trielle_aika(sanat_listana, aste)
-
-    def pilko_ja_laheta_trielle_aika(self, data: list, aste: int):
-        """
-        Ohjelmasta kopioitu trieen tallennusfunktio.
-        """
-        trie = Trie(Arpa())
-
-        if aste >= len(data):
-            return False
-        aika_sanalista = 0
-        aika_trie = 0
-        for i in range(len(data) - aste):
-            alku_sanalista = time()
-            sanalista = []
-            dataosoitin = i
-            while dataosoitin < i + aste + 1 and dataosoitin < len(data):
-                sanalista.append(data[dataosoitin])
-                dataosoitin += 1
-            aika_sanalista += time() - alku_sanalista
-            alku_trie = time()
-            trie.lisaa_sanalista(sanalista)
-            aika_trie += time() - alku_trie
-
-        self.tulokset["trieen"].append((aste, aika_sanalista, aika_trie))
-        print("sanlistojen muodostukseen kulunut aika", aika_trie)
-        print("Trieen talletusten aika", aika_sanalista)
-        return True
-
-    def teksti_listaksi_kuvaaja(self):
+    def teksti_listaksi_kuvaaja(self, tulokset):
         """
         Kuvaaja näyttää, miten sanojen määrä vaikuttaa tiedoston
         sisällön jäsentämiseen
         
         """
-        tapaukset = sorted(self.tulokset["jasennykset"], key=lambda a: a[2])
+        tapaukset = sorted(tulokset, key=lambda a: a[2])
         x = [x[2] for x in tapaukset]
         y = [y[1] for y in tapaukset]
 
@@ -165,36 +116,11 @@ class SuoritusTestaus:
         plt.legend()
         plt.show()
 
-    def trieen_tallennuksen_kuvaaja(self):
-        """
-        Kuvaajasta nähdään listan pilkkomiseen ja trieen tallentamiseen
-        kuluva aika.
-        """
-        x = [x[0] for x in self.tulokset["trieen"]]
-        y = [y[1] for y in self.tulokset["trieen"]]
-        plt.plot(x, y, label = "Pilkkominen")
-        plt.xlabel("Aste")
-        plt.ylabel("Aika (s)")
-
-        x = [x[0] for x in self.tulokset["trieen"]]
-        y = [y[2] for y in self.tulokset["trieen"]]
-        plt.plot(x, y, label = "Trieen")
-
-        x = [x[0] for x in self.tulokset["trieen"]]
-        y = [y[1]+y[2] for y in self.tulokset["trieen"]]
-        plt.plot(x, y, label="Yhteensä")
-
-        plt.xticks(range(len(self.tulokset["trieen"])))
-        plt.title(
-        "Tiedoston sanoista koostuvan listan pilkkominen asteen mittaisiin \nlistoihin ja niiden tallentaminen Trieen")
-        plt.legend()
-        plt.show()
-
-    def kaikki_tyhjaan_trieen_kuvaaja(self):
+    def kaikki_tyhjaan_trieen_kuvaaja(self, tulokset):
         """
         Kuvaaja tyhjään trieen tallentamisesta
         """
-        tulokset = sorted(self.tulokset["kaikki_tyhjaan_trieen"], key= lambda a: a[1])
+        tulokset = sorted(tulokset, key= lambda a: a[1])
         print("Aika yhteensä:", sum([x[2] for x in tulokset]))
         x = [x[1] for x in tulokset]
         y = [y[2] for y in tulokset]
@@ -204,11 +130,11 @@ class SuoritusTestaus:
         plt.title("Tallennukseen kuluva aika, kun trie on tyhjä")
         plt.show()
 
-    def kaikki_trieen_kuvaaja(self):
+    def kaikki_trieen_kuvaaja(self, tulokset):
         """
         Kuvaaja trieen tallentamisesta, kun sitä ei tyhjennetä välissä
         """
-        tulokset = self.tulokset["kaikki_trieen"].values()
+        tulokset = tulokset.values()
         tulokset = sorted(tulokset, key= lambda a: a[1])
         x = [x[1] for x in tulokset]
         y = [y[0] for y in tulokset]
